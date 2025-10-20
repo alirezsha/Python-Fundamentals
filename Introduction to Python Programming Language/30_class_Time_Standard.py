0956adc11263fa0f36c236792f4706351f13a7e3
Alireza Shabani <arshabani97@gmail.com>
2025-10-20 13:40:00 +0330

    Add files via upload

---
+++ Introduction to Python Programming Language/30_class_Time_Standard.py
@@ -0,0 +1,45 @@
+class Time:
+    def __init__(self, hour = 0, minute = 0, second = 0):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+        
+    def __str__(self):
+        return '%.2d:%.2d:%.2d' % (self.hour, self.minute, self.second)
+    
+    # def __add__(self, other):
+    #     seconds = self.time_to_int() + other.time_to_int()
+    #     return self.int_to_time(seconds)
+    
+    def __add__(self, other):
+        if isinstance(other, Time):
+            return self.add_time(other)
+        else:
+            return self.increase(other)
+        
+    def __radd__(self, other):
+        return self.__add__(other)
+    
+    def print_time(self):
+        print('%.2d:%.2d:%.2d' % (self.hour, self.minute, self.second))
+        
+    def increase(self, seconds):
+        seconds += self.time_to_int()
+        return self.int_to_time(seconds)
+    
+    def add_time(self, other):
+        seconds = self.time_to_int() + other.time_to_int()
+        return self.int_to_time(seconds)
+        
+    def time_to_int(self):
+        minutes = self.hour * 60 + self.minute
+        seconds = minutes * 60 + self.second
+        return seconds
+    
+    def int_to_time(self, seconds):
+        minutes, self.second = divmod(seconds, 60)
+        self.hour, self.minute = divmod(minutes, 60)
+        return self
+
+    def later(self, other):
+        return self.time_to_int() > other.time_to_int()
