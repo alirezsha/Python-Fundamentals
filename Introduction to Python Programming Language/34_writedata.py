90385bfc2c1e883ed31020e1fff15cc0b0a4191a
Alireza Shabani <arshabani97@gmail.com>
2025-10-25 15:26:29 +0330

    Add files via upload

---
+++ Introduction to Python Programming Language/34_writedata.py
@@ -0,0 +1,14 @@
+file_path = 'F:/Offline Courses/03 - Python/data_2w.txt'
+
+# with open(file_path, 'w') as f:
+#     f.write('Learning Python')
+    
+with open(file_path, 'w') as f:
+    f.write('Learning Python\n')
+    f.write('Programming in Python\n')
+    f.write('Python Programming Language\n')
+    
+with open(file_path, 'a') as f:
+    f.write('Data\n')
+    f.write('Data Analytic\n')
+    f.write('Data Science\n')
