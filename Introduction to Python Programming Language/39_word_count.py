3078d87a826ece6acba4922cb3e8cdf2fb83b4a9
Alireza Shabani <arshabani97@gmail.com>
2025-10-28 10:31:41 +0330

    Add files via upload

---
+++ Introduction to Python Programming Language/39_word_count.py
@@ -0,0 +1,17 @@
+def word_count(file_path):
+    try:
+        with open(file_path) as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print('The file does not exist.')
+        #pass
+    else:
+        words = contents.split()
+        number_of_words = len(words)
+        print(f'file {file_path} has {number_of_words} words.')
+
+file_path = ['a.txt',
+             'F:/Offline Courses/03 - Python/data_1.txt',
+             'F:/Offline Courses/03 - Python/data_2w.txt']
+for file in file_path:
+    word_count(file)
