489a9b1560644faa1b7fd34d7936116fcb748935
Alireza Shabani <arshabani97@gmail.com>
2025-10-29 22:49:46 +0330

    Update 40_numbers_json.py

--- Introduction to Python Programming Language/40_numbers_json.py
+++ Introduction to Python Programming Language/40_numbers_json.py
@@ -2,7 +2,7 @@ import json
 
 numbers = [13, 33, 36, 39, 66, 63, 99]
 
-file_path = 'F:/Offline Courses/03 - Python/numbers.json'
+file_path = 'numbers.json'
 
 with open(file_path, 'w') as f:
     json.dump(numbers, f)
