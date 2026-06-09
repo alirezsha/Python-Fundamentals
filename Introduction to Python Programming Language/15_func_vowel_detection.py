7da7507192d4fe5e7b2ca289f5b549de2f79ab66
Alireza Shabani <arshabani97@gmail.com>
2026-06-09 18:00:56 +0330

    Update 15_func_vowel_detection.py

--- Introduction to Python Programming Language/15_func_vowel_detection.py
+++ Introduction to Python Programming Language/15_func_vowel_detection.py
@@ -1,6 +1,6 @@
 def vowel_detection():
     """This function shows the vowels in the word."""
-    v = set('aeiou')
+    v = set('aeipu')
     w = input('please write a word: ')
 
     vowels = v.intersection(set(w))
